1296330fc23e248f461d50ae5ad60272454e1ef7
bzhunl <kursatturgut1352@gmail.com>
2022-07-15 05:33:18 -0700

    secrty

--- src/main/java/com/bzhilal/filografi/security/config/WebSecurityConfig.java
+++ src/main/java/com/bzhilal/filografi/security/config/WebSecurityConfig.java
@@ -1,4 +1,64 @@
 package com.bzhilal.filografi.security.config;
 
-public class WebSecurityConfig {
+import com.bzhilal.filografi.security.jwt.AuthEntryPointJwt;
+import com.bzhilal.filografi.security.jwt.AuthTokenFilter;
+import com.bzhilal.filografi.security.service.UserDetailsServiceImplement;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    private  final UserDetailsServiceImplement userDetailsService;
+    private  final AuthEntryPointJwt authEntryPointJwt;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().and().cors().disable().exceptionHandling().authenticationEntryPoint(authEntryPointJwt).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().antMatchers("/handmade/api/user/**")
+                .permitAll().anyRequest().authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers();
+    }
 }
--- src/main/java/com/bzhilal/filografi/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/bzhilal/filografi/security/jwt/AuthEntryPointJwt.java
@@ -1,5 +1,7 @@
 package com.bzhilal.filografi.security.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,15 @@ import java.io.IOException;
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
+    private static  final Logger logger= LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
 
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException authenticationException)
             throws IOException, ServletException {
+
+        logger.error("Unauthorized error:{}",authenticationException.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Error: Unauthorized");
 
     }
 }
